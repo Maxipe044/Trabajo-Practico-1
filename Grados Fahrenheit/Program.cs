29527e3780b647e4b548d0a41eefe0f54ef564a0
Máximo Emanuel Pedrozo <maximoemapedrozo@outlook.es>
2026-04-17 10:16:42 -0300

    Agregar archivos de proyecto.

---
+++ Grados Fahrenheit/Program.cs
@@ -0,0 +1,5 @@
+﻿Console.WriteLine("Grados Fahrenheit");
+Console.Write("Ingrese temperatura en grados Celsius: ");
+sbyte celsius = Convert.ToSByte(Console.ReadLine());
+short fahrenheit = (short)(celsius * 9 / 5 + 32);
+Console.WriteLine("La temperatura en grados Fahrenheit es: " + fahrenheit);
---
+++ Velocidad De La Luz/Program.cs
@@ -0,0 +1,9 @@
+﻿const double Velocidad_Luz_KM = 299_792.458;
+const double KM_Por_Milla_Nautica = 1.852;
+Console.WriteLine("Distancia recorrida por la luz");
+Console.Write("Ingrese el tiempo en segundos: ");
+double segundos = double.Parse(Console.ReadLine());
+double distanciaKm = Velocidad_Luz_KM * segundos;
+double distanciaMillasNauticas = distanciaKm / KM_Por_Milla_Nautica;
+Console.WriteLine($"Distancia en kilómetros:       {distanciaKm:N3} km");
+Console.WriteLine($"Distancia en millas náuticas:  {distanciaMillasNauticas:N3} mn");
