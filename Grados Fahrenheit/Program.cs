f0e3b06e397158450d5bb292b47a45a51daa55bb
Máximo Emanuel Pedrozo <maximoemapedrozo@outlook.es>
2026-04-17 11:03:03 -0300

    2/10

--- Grados Fahrenheit/Program.cs
+++ Grados Fahrenheit/Program.cs
@@ -1,5 +1,5 @@
 ﻿Console.WriteLine("Grados Fahrenheit");
 Console.Write("Ingrese temperatura en grados Celsius: ");
-sbyte celsius = Convert.ToSByte(Console.ReadLine());
-short fahrenheit = (short)(celsius * 9 / 5 + 32);
-Console.WriteLine("La temperatura en grados Fahrenheit es: " + fahrenheit);
+sbyte Celsius = Convert.ToSByte(Console.ReadLine());
+short Fahrenheit = (short)(Celsius * 9 / 5 + 32);
+Console.WriteLine("La temperatura en grados Fahrenheit es: " + Fahrenheit);
