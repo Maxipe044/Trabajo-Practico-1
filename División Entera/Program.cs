1d9b93a5a58e8a622652da9dbe200686bdf7d24e
Máximo Emanuel Pedrozo <maximoemapedrozo@outlook.es>
2026-04-17 11:58:00 -0300

    10/10

---
+++ División Entera/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.WriteLine("División Entera");
+Console.Write("Ingrese el primer número entero: ");
+int num1 = int.Parse(Console.ReadLine());
+Console.Write("Ingrese el segundo número entero: ");
+int num2 = int.Parse(Console.ReadLine());
+if (num2 == 0)
+{
+   Console.WriteLine("Error: no se puede dividir por cero.");
+    return;
+}
+int cociente = num1 / num2;
+int resto = num1 % num2;
+double divisionReal = (double)num1 / num2;
+Console.WriteLine("Resultados:");
+Console.WriteLine($"Cociente: {cociente}");
+Console.WriteLine($"Resto: {resto}");
+Console.WriteLine($"División real: {divisionReal}");
