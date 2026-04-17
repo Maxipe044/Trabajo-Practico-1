4535c8ccc7a5650237be3e67089d5967d4e0dce8
Máximo Emanuel Pedrozo <maximoemapedrozo@outlook.es>
2026-04-17 11:37:21 -0300

    8/10

---
+++ Facturación/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.WriteLine("Facturación");
+Console.Write("Ingrese el valor de su producto: ");
+double producto = double.Parse(Console.ReadLine());
+Console.Write("Ingrese el porcentaje de utilidad que deseado: ");
+double utilidad = double.Parse(Console.ReadLine());
+utilidad = utilidad / 100;
+double Iva = 0.21;
+double precioVenta = producto + (producto * utilidad) + (producto * Iva);
+Console.WriteLine($"El precio de venta del producto es: {precioVenta}");
